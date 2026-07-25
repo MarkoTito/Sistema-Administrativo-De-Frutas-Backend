c230566f857177e099072087bfe98ce9ab4d8792
Marko Tito <markojosheptitopena@gmail.com>
2026-07-25 16:14:23 -0500

    Mejoramiento del login 1 -backend

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -25,7 +26,44 @@ class UserController extends Controller
         }else{
             return response()->json(['mensaje'=> 'error al inicar', 'error' => '100']);
         }
-
-
     }
+
+    public function login2(Request $request)
+    {
+        $request->validate([
+            'p_email' => 'required|email',
+            'p_password' => 'required|string',
+        ]);
+
+        $email = $request->p_email;
+        $password = $request->p_password;
+
+        $usuarios = DB::select('SELECT * FROM sp_login(?)', [$email]);
+
+        if (empty($usuarios)) {
+            return response()->json(['mensaje' => 'Credenciales inválidas'], 401);
+        }
+
+        $usuario = $usuarios[0];
+
+        if (Hash::check($password, $usuario->password_hash)) {
+
+            // Genero elToken 
+            $userModel = new User((array) $usuario);
+            $token = $userModel->createToken('auth_token')->plainTextToken;
+
+            // limpio datos 
+            unset($usuario->password_hash);
+            
+            return response()->json([
+                'token' => $token,
+                'cliente' => $usuario
+            ], 200);
+        } else {
+            return response()->json(['mensaje' => 'Credenciales inválidas'], 401);
+        }
+    }
+
+
+
 }
